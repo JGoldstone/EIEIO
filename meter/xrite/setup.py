eac77c27796d1bfaa2d1cdf5e01b8e2065c76334
Joseph Goldstone <jgoldstone@arri.com>
2021-02-08 19:59:10 -0500

    Use /Library/Frameworks i1Pro[3] versions, and libi1ProAdapter.a from /usr/local/lib.

--- meter/xrite/setup.py
+++ meter/xrite/setup.py
@@ -2,12 +2,16 @@ from setuptools import setup, Extension
 
 # cf https://stackoverflow.com/questions/2584595/building-a-python-module-and-linking-it-against-a-macosx-framework
 from os import environ
-environ['LDFLAGS'] = '-F/Users/jgoldstone/Library/Frameworks -framework i1Pro -L/Users/jgoldstone/lib -li1ProAdapter'
+environ['LDFLAGS'] = '-F/Library/Frameworks -framework i1Pro3 -framework i1Pro -L/usr/local/lib -li1ProAdapter'
 
-iPAExtension = Extension('i1ProAdapter', include_dirs=['/Users/jgoldstone/include'], sources=['i1ProAdapterModule.c'])
+iPAExtension = Extension('i1ProAdapter',
+                         include_dirs=['/Users/jgoldstone/repos/git/x-rite/i1Pro_SDK/include',
+                                       '/Users/jgoldstone/repos/git/x-rite/i1Pro3_SDK/include'],
+                         sources=['i1ProAdapterModule.c'])
 
 # When this script is invoked, the invocation needs to have a single argument 'install' passed on
-# the invocation. For PyCharm, this means setting the value of the 'Parameters' field to 'install'.
+# the invocation. For PyCharm, this means setting the value of the relevant configuration's
+# 'Parameters' field to 'install'.
 setup(
     name="i1ProAdapter",
     version="0.9",
